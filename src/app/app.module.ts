a58d249c127cad0d71d90642d72dc02912b8693e
Gabriel Peixoto <gabriel.peixoto@usemobile.xyz>
2025-07-27 03:16:24 -0300

    🚧 : Added config module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { IsAdminGuard } from 'src/common/guards/is.admin.guard';
@@ -9,15 +10,16 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      database: 'postgres',
-      password: '123456',
-      autoLoadEntities: true,
-      synchronize: true,
+      type: process.env.DATABASE_TYPE as 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE,
+      autoLoadEntities: process.env.DATABASE_AUTOLOAD_ENTITIES === 'true',
+      synchronize: process.env.DATABASE_SYNCHRONIZE === 'true',
     }),
     MessagesModule,
     UsersModule,
