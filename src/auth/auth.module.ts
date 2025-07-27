d0fa289aebb04a3ec9295122a8573895710ec675
Gabriel Peixoto <gabriel.peixoto@usemobile.xyz>
2025-07-27 20:32:28 -0300

    🚧 : Created bcrypt service

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,14 @@
+import { Global, Module } from '@nestjs/common';
+import { BCryptService } from './hashing/bcrypt.service';
+import { HashingService } from './hashing/hash.service';
+
+@Global()
+@Module({
+  providers: [
+    {
+      provide: HashingService,
+      useClass: BCryptService,
+    },
+  ],
+})
+export class AuthModule {}
