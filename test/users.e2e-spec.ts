9b5cfd4628a80f1ccfe14888844ee43c72a3bc14
Gabriel Peixoto <gabriel.peixoto@usemobile.xyz>
2025-09-28 17:49:39 -0300

    🚧 : Creating users e2e test

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -1,10 +1,13 @@
 import { HttpStatus, INestApplication } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { resolve } from 'path';
 import pipesConfig from 'src/app/config/pipes.config';
 import { AuthModule } from 'src/auth/auth.module';
+import jwtConfig from 'src/auth/config/jwt.config';
 import { MessagesModule } from 'src/messages/messages.module';
 import { UsersModule } from 'src/users/users.module';
 import * as request from 'supertest';
@@ -16,6 +19,9 @@ describe('AppController (e2e)', () => {
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             imports: [
+                ConfigModule.forRoot({ envFilePath: '.env.test' }),
+                ConfigModule.forFeature(jwtConfig),
+                JwtModule.registerAsync(jwtConfig.asProvider()),
                 TypeOrmModule.forRoot({
                     type: 'postgres',
                     host: 'localhost',
@@ -55,6 +61,54 @@ describe('AppController (e2e)', () => {
                 password: '123456'
             };
             const response = await request(app.getHttpServer()).post('/users').send(createUserDto).expect(HttpStatus.CREATED)
+            expect(response.body).toEqual(
+                {
+
+                    email: createUserDto.email,
+                    passwordHash: expect.any(String),
+                    name: createUserDto.name,
+                    active: true,
+                    createdAt: expect.any(String),
+                    updatedAt: expect.any(String),
+                    picture: '',
+                    id: expect.any(Number),
+                    routePolicies: []
+                }
+            )
+        });
+    });
+
+    describe('/users/:id (GET)', () => {
+
+        it('Should return unauthorized error when an user is not logged in', async () => {
+
+            const userResponse = await request(app.getHttpServer()).post('/users').send({
+                email: 'a@a.com',
+                name: 'Gabriel Peixoto',
+                password: '123456'
+            }).expect(HttpStatus.CREATED)
+
+            await request(app.getHttpServer()).get('/users/' + userResponse.body.id).expect(HttpStatus.UNAUTHORIZED);
+
+        });
+
+        it('Should return User when it is logged in', async () => {
+            const createUserDto = {
+                email: 'a@a.com',
+                name: 'Gabriel Peixoto',
+                password: '123456'
+            };
+            const userResponse = await request(app.getHttpServer()).post('/users').send(createUserDto).expect(HttpStatus.CREATED);
+
+            const loginResponse = await request(app.getHttpServer()).post('/auth').send({
+                email: createUserDto.email,
+                password: createUserDto.password
+            });
+
+            const response = await request(app.getHttpServer()).get('/users/' + userResponse.body.id)
+                .set('Authorization', `Bearer ${loginResponse.body.accessToken}`)
+                .expect(HttpStatus.OK);
+
             expect(response.body).toEqual(
                 {
 
